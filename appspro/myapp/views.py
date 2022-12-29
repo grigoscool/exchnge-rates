3bc77acec662dd143fb7ed2da18266f4f08b391b
Sergey Grigorev <grigoscool@mail.ru>
2022-12-29 14:06:32 +0300

    v 1.0 exchenge money

---
+++ appspro/myapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import requests
+
+
+
+def index(request):
+
+    url = "https://v6.exchangerate-api.com/v6/91697ffd3e1ee0d705b57ffe/latest/USD"
+    headers = {
+        "apikey": "91697ffd3e1ee0d705b57ffe"
+    }
+    response = requests.get(url, headers=headers).json()
+    result = response.get('conversion_rates')
+
+    if request.method == 'GET':
+        context = {'curs': result}
+        return render(request, 'myapp/index.html', context)
+
+    if request.method == "POST":
+        amount = float(request.POST.get('amount'))
+        form_cur = request.POST.get("form_cur")
+        out_cur = request.POST.get("out_cur")
+
+        total = round(((result[out_cur]/result[form_cur])*amount),2)
+        context = {
+            'curs': result,
+            'total': total,
+        }
+        return render(request, 'myapp/index.html', context)
